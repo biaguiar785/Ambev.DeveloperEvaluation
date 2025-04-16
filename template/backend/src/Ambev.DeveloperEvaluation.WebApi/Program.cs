898c487427698223935900f1e61a40077f1db999
Bianca Aguiar <biaguiar785@gmail.com>
2025-04-16 00:19:34 -0300

    Adicionar novos repositórios e HttpContextAccessor
    Adicionados os repositórios `ISaleRepository`, `IBranchRepository`, `ISaleItemRepository` e `IProductRepository` ao `InfrastructureModuleInitializer.cs`, registrando-os como serviços no contêiner de injeção de dependência. Removida a importação de `Microsoft.Extensions.Configuration`, que não era mais necessária.
    
    Incluída a linha `builder.Services.AddHttpContextAccessor();` no `Program.cs` para permitir o acesso ao contexto HTTP em toda a aplicação.

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -25,6 +25,7 @@ public class Program
 
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddHttpContextAccessor();
 
             builder.AddBasicHealthChecks();
             builder.Services.AddSwaggerGen();
